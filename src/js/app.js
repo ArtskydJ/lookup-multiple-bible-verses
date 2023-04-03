c072a79eac409ec1408a5e19a47e037761ec6dbb
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2023-04-03 15:12:19 -0500

    Play around with a permalink (See #5)

--- src/js/app.js
+++ src/js/app.js
@@ -9,18 +9,27 @@ async function refresh() {
 
 	const ranges = extract_ranges_from_text(text)
 
-	const html_chunks = await Promise.all(
+	const bible_passages = await Promise.all(
 		ranges.map(async range => {
 			try {
 				const { reference, text } = await fetch_bible_passage_from_range(range)
-				return `<span class="verse"><span class="ref">${ reference }</span>${ text }</span>`
-			} catch (e) {
-				return `<span class="err">${ e.message }</span>`
+				return { error: null, reference, text}
+			} catch (error) {
+				return { error }
 			}
-		}),
+		})
 	)
 
-	output.innerHTML = html_chunks.join('')
+	const passage_html = bible_passages.map(({ error, reference, text }) => error
+		? `<span class="err">${ error.message }</span>`
+		: `<span class="verse"><span class="ref">${ reference }</span>${ text }</span>`
+	).join('')
+
+	const hash_fragment = bible_passages.filter(({ error }) => !error).map(({ reference }) => reference).join('/')
+	const permalink_html = `<a class="permalink" href="#${encodeURIComponent(hash_fragment)}">Permalink</a>`
+	console.log(permalink_html)
+
+	output.innerHTML = passage_html // + permalink_html
 }
 
 textarea.oninput = refresh
